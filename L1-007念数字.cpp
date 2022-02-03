fd717f43c2e0dbcfde73892bc723ca8d586c500f
yuyu-work-space <ykdzszcml@163.com>
2022-02-03 23:39:36 +0800

    TianTi Question

---
+++ L1-007念数字.cpp
@@ -0,0 +1,152 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+
+    if(str.at(0) == '-')
+    {
+        cout << "fu" << " ";
+        for(int i = 0; i < str.size(); i ++)
+        {
+            if(i == str.size() - 1)
+            {
+                if(str.at(i) == '0')
+            {
+                cout << "ling";
+            }else if(str.at(i) == '1')
+            {
+                cout << "yi";
+            }else if(str.at(i) == '2')
+            {
+                cout << "er" << " ";
+            }else if(str.at(i) == '3')
+            {
+                cout << "san";
+            }else if(str.at(i) == '4')
+            {
+                cout << "si";
+            }else if(str.at(i) == '5')
+            {
+                cout << "wu";
+            }else if(str.at(i) == '6')
+            {
+                cout << "liu";
+            }else if(str.at(i) == '7')
+            {
+                cout << "qi";
+            }else if(str.at(i) == '8')
+            {
+                cout << "ba";
+            }else if(str.at(i) == '9')
+            {
+                cout << "jiu";
+            }
+            }else 
+            if(str.at(i) == '0')
+            {
+                cout << "ling" << " ";
+            }else if(str.at(i) == '1')
+            {
+                cout << "yi" << " ";
+            }else if(str.at(i) == '2')
+            {
+                cout << "er" << " ";
+            }else if(str.at(i) == '3')
+            {
+                cout << "san" << " ";
+            }else if(str.at(i) == '4')
+            {
+                cout << "si" << " ";
+            }else if(str.at(i) == '5')
+            {
+                cout << "wu" << " ";
+            }else if(str.at(i) == '6')
+            {
+                cout << "liu" << " ";
+            }else if(str.at(i) == '7')
+            {
+                cout << "qi" << " ";
+            }else if(str.at(i) == '8')
+            {
+                cout << "ba" << " ";
+            }else if(str.at(i) == '9')
+            {
+                cout << "jiu" << " ";
+            }
+        }
+    }else 
+    {
+        for(int i = 0; i < str.size(); i ++)
+        {
+            if(i == str.size() - 1)
+            {
+                if(str.at(i) == '0')
+            {
+                cout << "ling";
+            }else if(str.at(i) == '1')
+            {
+                cout << "yi";
+            }else if(str.at(i) == '2')
+            {
+                cout << "er" << " ";
+            }else if(str.at(i) == '3')
+            {
+                cout << "san";
+            }else if(str.at(i) == '4')
+            {
+                cout << "si";
+            }else if(str.at(i) == '5')
+            {
+                cout << "wu";
+            }else if(str.at(i) == '6')
+            {
+                cout << "liu";
+            }else if(str.at(i) == '7')
+            {
+                cout << "qi";
+            }else if(str.at(i) == '8')
+            {
+                cout << "ba";
+            }else if(str.at(i) == '9')
+            {
+                cout << "jiu";
+            }
+            }else 
+            if(str.at(i) == '0')
+            {
+                cout << "ling" << " ";
+            }else if(str.at(i) == '1')
+            {
+                cout << "yi" << " ";
+            }else if(str.at(i) == '2')
+            {
+                cout << "er" << " ";
+            }else if(str.at(i) == '3')
+            {
+                cout << "san" << " ";
+            }else if(str.at(i) == '4')
+            {
+                cout << "si" << " ";
+            }else if(str.at(i) == '5')
+            {
+                cout << "wu" << " ";
+            }else if(str.at(i) == '6')
+            {
+                cout << "liu" << " ";
+            }else if(str.at(i) == '7')
+            {
+                cout << "qi" << " ";
+            }else if(str.at(i) == '8')
+            {
+                cout << "ba" << " ";
+            }else if(str.at(i) == '9')
+            {
+                cout << "jiu" << " ";
+            }
+        }
+    }
+    
+}
